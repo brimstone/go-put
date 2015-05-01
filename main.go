6324158f9c31fbe399133f75f4096750979ad066
Matt <brimstone@the.narro.ws>
2015-05-01 00:41:08 -0400

    Almost finished conversion to tree structure instead of a simple list.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
@@ -11,15 +12,17 @@ import (
 )
 
 type dataItem struct {
-	mime  string
-	value string
+	mime     string
+	value    string
+	children map[string]dataItem
 }
 
-var data map[string]dataItem
+var root dataItem
 
-func list() ([]string, error) {
+func list(base dataItem) ([]string, error) {
+	log.Printf("base: %#s\n", base)
 	keys := make([]string, 0)
-	for k := range data {
+	for k := range base.children {
 		path := strings.Split(k, "/")
 		if path[len(path)-1][0] != '\x2e' {
 			keys = append(keys, k)
@@ -34,11 +37,40 @@ func returnDataItem(w http.ResponseWriter, object dataItem) error {
 	return nil
 }
 
-func getDataItem(path []string) (dataItem, error) {
-	if object, ok := data[strings.Join(path, "/")]; ok {
+func getDataItem(base dataItem, path []string) (dataItem, error) {
+	//log.Println("Trying to get", path)
+	log.Printf("looking for child: %s, %#v\n", path[0], base)
+	if object, ok := base.children[path[0]]; ok {
+		if len(path) > 1 {
+			return getDataItem(object, path[1:])
+		}
 		return object, nil
 	}
 	return dataItem{}, fmt.Errorf("No such key")
+}
+
+func makeParents(base *dataItem, path []string) *dataItem {
+	// exit conditions: len(path) < 2
+	if base.children == nil {
+		base.children = make(map[string]dataItem)
+	}
+	if len(path) == 1 {
+		return base
+	}
+	if object, ok := base.children[path[0]]; ok {
+		return &object
+	} else {
+		kid := makeParents(&object, path[1:])
+		base.children[path[0]] = *kid
+		return kid
+	}
+	return nil
+}
+
+func storeDataItem(path []string, object dataItem) error {
+	parent := makeParents(&root, path)
+	parent.children[path[len(path)-1]] = object
+	return nil
 }
 
 func handleData(w http.ResponseWriter, r *http.Request) {
@@ -50,17 +82,25 @@ func handleData(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
 		if path[len(path)-1] == "" {
-			if object, err := getDataItem(append(path[:len(path)-1], ".index")); err == nil {
+			//log.Println("Looking for", path, ".index")
+			if object, err := getDataItem(root, append(path[:len(path)-1], ".index")); err == nil {
 				returnDataItem(w, object)
 				return
 			} else {
-				keys, _ := list()
-				out, _ := json.Marshal(keys)
-				w.Header().Add("Content-Type", "application/json; charset=utf-8")
-				fmt.Fprintf(w, string(out))
-				return
+				//log.Println("Didn't find .index, generating one")
+				if object, err := getDataItem(root, path[:len(path)-1]); err == nil {
+					//log.Println("Returning auto generated index")
+					keys, _ := list(object)
+					out, _ := json.Marshal(keys)
+					w.Header().Add("Content-Type", "application/json; charset=utf-8")
+					fmt.Fprintf(w, string(out))
+					return
+				} else {
+					http.NotFound(w, r)
+					return
+				}
 			}
-		} else if object, err := getDataItem(path); err == nil {
+		} else if object, err := getDataItem(root, path); err == nil {
 			returnDataItem(w, object)
 			return
 		}
@@ -75,16 +115,17 @@ func handleData(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		data[strings.Join(path, "/")] = dataItem{
+		err = storeDataItem(path, dataItem{
 			value: string(body),
 			mime:  r.Header.Get("Content-Type"),
+		})
+		if err != nil {
+			fmt.Fprint(w, "There was an error saving the item.", err.Error())
+			return
 		}
+
 		return
 	}
-}
-
-func init() {
-	data = make(map[string]dataItem)
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -12,19 +12,25 @@ func TestHandleData(t *testing.T) {
 }
 
 func TestIndex(t *testing.T) {
+	t.Log("Trying empty index.")
 	req, _ := saverequest.FakeRequest("GET", "/data/", map[string]string{}, "")
 	w := httptest.NewRecorder()
 	handleData(w, req)
-	if w.Body.String() != "[]" {
-		t.Errorf("Unable to get the index request")
+	if w.Code != 404 && w.Body.String() != "" {
+		t.Errorf("Unable to get empty request")
+		t.Errorf("%d: %s", w.Code, w.Body.String())
 		return
+	} else {
+		t.Log("Got generated index")
 	}
 
+	t.Log("Trying to put an index file")
 	req, _ = saverequest.FakeRequest("POST", "/data/.index", map[string]string{"Content-Type": "text/html"}, "This is the index")
 	w = httptest.NewRecorder()
 	handleData(w, req)
 	if w.Body.String() != "" {
 		t.Errorf("Unable to save new index request")
+		t.Errorf("%d: %s", w.Code, w.Body.String())
 		return
 	}
 
@@ -32,7 +38,8 @@ func TestIndex(t *testing.T) {
 	w = httptest.NewRecorder()
 	handleData(w, req)
 	if w.Body.String() != "This is the index" {
-		t.Errorf("Unable to save new index request")
+		t.Errorf("Unable to get new index request")
+		t.Errorf("%d: %s", w.Code, w.Body.String())
 		return
 	}
 
@@ -40,7 +47,8 @@ func TestIndex(t *testing.T) {
 	w = httptest.NewRecorder()
 	handleData(w, req)
 	if w.Body.String() != "This is the index" {
-		t.Errorf("Unable to save new index request")
+		t.Errorf("Unable to generated index")
+		t.Errorf("%d: %s", w.Code, w.Body.String())
 		return
 	}
 
