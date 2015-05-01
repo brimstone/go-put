08fb0bd55fc453bdeed2e186daed79630685cde7
Matt <brimstone@the.narro.ws>
2015-04-30 22:06:28 -0400

    Made .index logic work right, and testing.

--- main.go
+++ main.go
@@ -20,9 +20,25 @@ var data map[string]dataItem
 func list() ([]string, error) {
 	keys := make([]string, 0)
 	for k := range data {
-		keys = append(keys, k)
+		path := strings.Split(k, "/")
+		if path[len(path)-1][0] != '\x2e' {
+			keys = append(keys, k)
+		}
 	}
 	return keys, nil
+}
+
+func returnDataItem(w http.ResponseWriter, object dataItem) error {
+	w.Header().Add("Content-Type", object.mime+"; charset=utf-8")
+	fmt.Fprint(w, object.value)
+	return nil
+}
+
+func getDataItem(path []string) (dataItem, error) {
+	if object, ok := data[strings.Join(path, "/")]; ok {
+		return object, nil
+	}
+	return dataItem{}, fmt.Errorf("No such key")
 }
 
 func handleData(w http.ResponseWriter, r *http.Request) {
@@ -33,14 +49,19 @@ func handleData(w http.ResponseWriter, r *http.Request) {
 	path = path[1:]
 
 	if r.Method == "GET" {
-		if string(path[len(path)-1]) == "/" {
-			keys, _ := list()
-			out, _ := json.Marshal(keys)
-			w.Header().Add("Content-Type", "application/json; charset=utf-8")
-			fmt.Fprintf(w, string(out))
-		} else if object, ok := data[strings.Join(path, "/")]; ok {
-			w.Header().Add("Content-Type", object.mime+"; charset=utf-8")
-			fmt.Fprint(w, object.value)
+		if path[len(path)-1] == "" {
+			if object, err := getDataItem(append(path[:len(path)-1], ".index")); err == nil {
+				returnDataItem(w, object)
+				return
+			} else {
+				keys, _ := list()
+				out, _ := json.Marshal(keys)
+				w.Header().Add("Content-Type", "application/json; charset=utf-8")
+				fmt.Fprintf(w, string(out))
+				return
+			}
+		} else if object, err := getDataItem(path); err == nil {
+			returnDataItem(w, object)
 			return
 		}
 		http.NotFound(w, r)
@@ -62,9 +83,12 @@ func handleData(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func init() {
+	data = make(map[string]dataItem)
+}
+
 func main() {
 
-	data = make(map[string]dataItem)
 	saverequest.WriteRequests = true
 	http.HandleFunc("/data/", handleData)
 	http.ListenAndServe(":8000", nil)
